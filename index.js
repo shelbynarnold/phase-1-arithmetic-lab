d49685ab802dce24a3dcedf27e76f34a04a09b2c
shelby <shelbynarnold@yahoo.com>
2023-02-22 09:38:13 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 2
+let num4 = 2
+let mod = num3 + num4
+
+const max = Math.max(1, 2, 20)
